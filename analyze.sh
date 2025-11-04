aecd0c8f4ec88ba2674e44b2bfd891442678d2ce
azjezz <azjezz@protonmail.com>
2025-11-04 16:04:07 +0100

    more?
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- analyze.sh
+++ analyze.sh
@@ -4,8 +4,8 @@ set -e
 echo "Building in release mode..."
 cargo build --release
 
-echo "Downloading packages 1000-1500..."
-./target/release/php-syntax-analyzer --keyword using --min 1000 --max 1500 > /dev/null
+echo "Downloading packages 10000-15000..."
+./target/release/php-syntax-analyzer --keyword using --min 10000 --max 15000
 
 echo "Analyzing for 'let' keyword..."
 ./target/release/php-syntax-analyzer --keyword let --skip-download > results/let.txt
